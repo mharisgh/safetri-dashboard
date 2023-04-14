e2016833c586bd0315c852ab8698d5e566174447
Haris <hellomharis@gmail.com>
2023-04-14 14:45:19 +0530

    cards updated

--- script.js
+++ script.js
@@ -36,67 +36,40 @@ inputBox.addEventListener('change', function (e) {
 
 
 // arrear speedometer script
-// const vacancy = document.querySelector('#vacancy')
-// const target = document.querySelector('#target');
-// const input = document.getElementById('#inputBox')
-// const totalStudents = 1200;
-// let currentStudents = document.getElementById('currentStudents');
+const arrear = document.querySelector('#arrear')
+const targetArrear = document.querySelector('#target-arrear');
+const inputArrear = document.getElementById('#inputBoxArrear')
+const totalStudentsArrear = 1200;
+let currentStudentsArrear = document.getElementById('currentStudents');
 
-// var circle = document.getElementById('round-progress');
-// var radius = circle.r.baseVal.value;
-// var circumference = radius * 2 * Math.PI;
+var circjle = document.getElementById('round-progress-arrear');
+var radius = circle.r.baseVal.value;
+var circumference = radius * 2 * Math.PI;
 
-// circle.style.strokeDasharray = `${circumference} ${circumference}`;
-// circle.style.strokeDashoffset = `${circumference}`;
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = `${circumference}`;
 
-// function setProgress(percent) {
-//     const offset = circumference - percent / 126 * circumference;
-//     circle.style.strokeDashoffset = offset;
-// }
-
-// const inputBox = document.getElementById('inputBox');
-// setProgress(inputBox.value);
-
-// inputBox.addEventListener('change', function (e) {
-//     if (inputBox.value < 101 && inputBox.value > -1) {
-//         setProgress(inputBox.value);
-
-//     let percentageToStudentsEnrolled = inputBox.value /  100 * totalStudents;
-//     const val = inputBox.value;
-//     const rotAmount = 186 + ((val / 100) * 284);
-//     currentStudents.innerHTML = percentageToStudentsEnrolled;
-//     vacancy.innerHTML = totalStudents - percentageToStudentsEnrolled;
-//     target.style.transform = `rotate(${rotAmount}deg)`;
-
-//     }
-// });
-
-
-
-let tabsContainer = document.querySelector("#tabs");
-
-let tabTogglers = tabsContainer.querySelectorAll("a");
-console.log(tabTogglers);
-
-tabTogglers.forEach(function(toggler) {
-toggler.addEventListener("click", function(e) {
-e.preventDefault();
-
-let tabName = this.getAttribute("href");
-
-let tabContents = document.querySelector("#tab-contents");
-
-for (let i = 0; i < tabContents.children.length; i++) {
-
-tabTogglers[i].parentElement.classList.remove("border-blue-400", "border-b",  "-mb-px", "opacity-100");  tabContents.children[i].classList.remove("hidden");
-if ("#" + tabContents.children[i].id === tabName) {
-continue;
+function setProgress(percent) {
+    const offset = circumference - percent / 126 * circumference;
+    circle.style.strokeDashoffset = offset;
 }
-tabContents.children[i].classList.add("hidden");
 
-}
-e.target.parentElement.classList.add("border-blue-400", "border-b-4", "-mb-px", "opacity-100");
-});
+const inputBoxArrear = document.getElementById('inputBoxArrear');
+setProgress(inputBoxArrear.value);
+
+inputBoxArrear.addEventListener('change', function (e) {
+    if (inputBoxArrear.value < 101 && inputBoxArrear.value > -1) {
+        setProgress(inputBoxArrear.value);
+
+    let percentageToStudentsEnrolled = inputBoxArrear.value /  100 * totalStudents;
+    const val = inputBoxArrear.value;
+    const rotAmount = 186 + ((val / 100) * 284);
+    // currentStudents.innerHTML = percentageToStudentsEnrolled;
+    // vacancy.innerHTML = totalStudents - percentageToStudentsEnrolled;
+    targetArrear.style.transform = `rotate(${rotAmount}deg)`;
+    }
 });
 
-document.getElementById("default-tab").click();
+
+
+
